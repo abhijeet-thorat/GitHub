ff187365e29e9e352e4b02233f3e32dab56dc6ff
Abhijeet <abhijeetthorat3698@gmail.com>
2023-03-30 23:21:03 +0530

    second commit

--- Sample.java
+++ Sample.java
@@ -2,6 +2,7 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Good Morning");
         System.out.println("Jai Shree Ram");
+        System.out.println("Have a nice Day");
     }
     
 }
